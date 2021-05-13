768ee977c7dffdfe81073c04b6aaac8bf5519211
Rafael Zufi <rzufil@gmail.com>
2021-05-13 16:09:34 -0300

    Adding other models to db

--- models/index.js
+++ models/index.js
@@ -6,6 +6,9 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
+db.degree = require("./degree.model.js")(mongoose);
 db.experience = require("./experience.model.js")(mongoose);
+db.language = require("./language.model.js")(mongoose);
+db.skill = require("./skill.model.js")(mongoose);
 
 module.exports = db;
